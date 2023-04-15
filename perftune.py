ea3626d1202fd08745038f24fa4b3fd0c9b30367
GuinnessShep <108033341+GuinnessShep@users.noreply.github.com>
2023-04-15 12:04:51 -0700

    Update perftune.py

--- perftune.py
+++ perftune.py
@@ -71,15 +71,38 @@ function display_cpu_info() {
 }
 
 function overclock_gpu() {
-    # ... (Overclock GPU function as in the previous revision)
+    echo "Enter GPU Clock Offset (MHz):"
+    read gpu_offset
+    echo "Enter Memory Clock Offset (MHz):"
+    read mem_offset
+    echo "Setting GPU and Memory Clock Offsets..."
+    nvidia-settings -a "[gpu:0]/GPUGraphicsClockOffset[3]=$gpu_offset" -a "[gpu:0]/GPUMemoryTransferRateOffset[3]=$mem_offset"
+    echo "Overclocking successful."
 }
 
-function overclock_cpu() {
-    # ... (Overclock CPU function as in the previous revision)
+function overclock_cpu_advanced() {
+    echo "WARNING: This operation may cause system instability and void your warranty. Proceed at your own risk."
+    echo "Enter the desired CPU multiplier (e.g., 40 for 4.0 GHz, or 45 for 4.5 GHz):"
+    read cpu_multiplier
+    for cpu in /dev/cpu/[0-9]*; do
+        wrmsr -p "$(basename $cpu)" 0x194 0x$(printf "%X" $((cpu_multiplier << 8)))
+    done
+    echo "CPU multiplier set to $cpu_multiplier."
+    read -p "Press enter to continue..."
+}
+
+function overclock_memory() {
+    echo "WARNING: This operation may cause system instability and void your warranty. Proceed at your own risk."
+    echo "Enter the desired memory clock speed in MHz (e.g., 3200):"
+    read mem_clock
+    numactl --interleave=all --membind=0 --physcpubind=0-$(($(nproc)-1)) --localalloc taskset -c 0-$(($(nproc)-1)) dmidecode -t memory | grep -i "speed" | sed "s/Speed: //g" | awk '{if ($1 < '$mem_clock') print "Warning: The desired memory clock speed ("'$mem_clock'" MHz) exceeds the maximum supported speed for at least one memory module."}'
+    echo "Memory overclocking is not supported directly through this script. Please follow your motherboard's documentation and adjust memory settings through BIOS/UEFI."
+    read -p "Press enter to continue..."
 }
 
 function reset_gpu_settings() {
-    # ... (Reset GPU settings function as in the previous revision)
+    nvidia-settings -a "[gpu:0]/GPUGraphicsClockOffset[3]=0" -a "[gpu:0]/GPUMemoryTransferRateOffset[3]=0"
+    echo "GPU settings have been reset."
 }
 
 function adjust_gpu_power_limit() {
@@ -129,6 +152,73 @@ function toggle_hugepages() {
     read -p "Press enter to continue..."
 }
 
+function adjust_gpu_application_clocks() {
+    echo "Select a preset for GPU application clocks:"
+    select preset in "low" "medium" "high" "custom"; do
+        case $preset in
+            low)
+                core_clock="600"
+                mem_clock="1100"
+                break
+                ;;
+            medium)
+                core_clock="900"
+                mem_clock="1300"
+                break
+                ;;
+            high)
+                core_clock="1200"
+                mem_clock="1500"
+                break
+                ;;
+            custom)
+                echo "Enter desired core clock (in MHz):"
+                read core_clock
+                echo "Enter desired memory clock (in MHz):"
+                read mem_clock
+                break
+                ;;
+        esac
+    done
+    nvidia-settings -a "[gpu:0]/GPUGraphicsClockOffset[3]=$core_clock" -a "[gpu:0]/GPUMemoryTransferRateOffset[3]=$mem_clock"
+    echo "GPU application clocks set to Core: $core_clock MHz, Memory: $mem_clock MHz."
+    read -p "Press enter to continue..."
+}
+
+function set_swappiness() {
+    echo "Enter desired swappiness value (0-100):"
+    read swappiness
+    sysctl -w vm.swappiness=$swappiness
+    echo "Swappiness value set to $swappiness."
+    read -p "Press enter to continue..."
+}
+
+function optimize_disk_cache_pressure() {
+    echo "Enter desired disk cache pressure value (0-100):"
+    read cache_pressure
+    sysctl -w vm.vfs_cache_pressure=$cache_pressure
+    echo "Disk cache pressure value set to $cache_pressure."
+    read -p "Press enter to continue..."
+}
+
+function configure_thp() {
+    echo "Select THP mode:"
+    select mode in "always" "madvise" "never"; do
+        echo $mode > /sys/kernel/mm/transparent_hugepage/enabled
+        echo "Transparent Huge Pages mode set to $mode."
+        read -p "Press enter to continue..."
+        break
+    done
+}
+
+function optimize_readahead() {
+    echo "Enter desired readahead value (in kilobytes):"
+    read readahead_kb
+    blockdev --setra $readahead_kb /dev/sda
+    echo "Readahead value set to $readahead_kb KB."
+    read -p "Press enter to continue..."
+}
+
 function set_io_scheduler() {
     echo "Select I/O scheduler:"
     select io_scheduler in "noop" "deadline" "cfq" "bfq" "mq-deadline" "kyber"; do
@@ -141,50 +231,4 @@ function set_io_scheduler() {
     done
 }
 
-# Main loop for the menu
-while true; do
-    menu
-    case $choice in
-        1)
-            display_gpu_info
-            read -p "Press enter to continue..."
-        ;;
-        2)
-            display_cpu_info
-            read -p "Press enter to continue..."
-        ;;
-        3)
-            overclock_gpu
-        ;;
-        4)
-            overclock_cpu
-        ;;
-        5)
-            reset_gpu_settings
-        ;;
-        6)
-            adjust_gpu_power_limit
-        ;;
-        7)
-            toggle_gpu_persistence_mode
-        ;;
-        8)
-            set_cpu_governor
-        ;;
-        9)
-            toggle_hugepages
-        ;;
-        10)
-            set_io_scheduler
-        ;;
-        11)
-            echo "Exiting..."
-            exit 0
-        ;;
-        *)
-            echo "Invalid choice, try again."
-            read -p "Press enter to continue..."
-        ;;
-    esac
-done
 
